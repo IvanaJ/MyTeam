27671eaccd37554aaeadcf655daac775686cf424
Ivana Jakimovska <ivana.jakimovska@nmc.com.mk>
2017-03-22 11:19:03 +0100

    Created migrations, model and seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,24 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+//$factory->define(App\User::class, function (Faker\Generator $faker) {
+//    static $password;
+//
+//    return [
+//        'name' => $faker->name,
+//        'email' => $faker->unique()->safeEmail,
+//        'password' => $password ?: $password = bcrypt('secret'),
+//        'remember_token' => str_random(10),
+//    ];
+//});
+
+
+$factory->define(App\Project::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'title' => $faker->text(),
+        'description' => $faker->text(),
+
     ];
 });

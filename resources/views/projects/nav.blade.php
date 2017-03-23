ff89b3edf166e2a4c3f3f6d328c940361e56e5ed
Ivana Jakimovska <ivana.jakimovska@nmc.com.mk>
2017-03-23 14:50:19 +0100

    Made only signed in users can add projects

--- resources/views/projects/nav.blade.php
+++ resources/views/projects/nav.blade.php
@@ -4,14 +4,14 @@
             <a class="nav-link active" href="/">Home</a>
             <a class="nav-link" href="/projects">Projects</a>
             @if(Auth::check())
-            <a class="nav-link" href="/add-project">Add Project</a>
-            @endif
-            <a class="nav-link" href="/register">Register</a>
-            <a class="nav-link ml-auto" style="left: 25%;" href="/login">Login</a>
-            <a class="nav-link ml-auto" href="/logout">Logout</a>
-            @if(Auth::check())
+                <a class="nav-link" href="/add-project">Add Project</a>
+                <a class="nav-link " href="/logout" style="left: 70%;">Logout</a>
                 <a class="nav-link ml-auto" href="#">Welcome, {{Auth::user()->name}}</a>
+            @else
+                <a class="nav-link" href="/register" style="left: 90%;">Register</a>
+                <a class="nav-link ml-auto" style="left: 25%;" href="/login">Login</a>
             @endif
+
         </nav>
     </div>
 </div>
